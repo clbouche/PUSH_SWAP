66aaaa4c8ab42ccdf832dd469fa37263ccbb2565
clbouche <57404773+clbouche@users.noreply.github.com>
2021-06-30 10:28:34 +0200

    Add files via upload

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -7,17 +7,7 @@
 # define INT_MIN -2147483648
 # define SUCCESS 1
 # define FAILURE 0
-
-/*A SUPPRIMER AVANT DE PUSH*/
-# define ERR "Error : "
-# define ERR_ARG "There are not enough arguments to start the program"
-# define ERR_NUMBER "This list is not just made up of numbers"
-# define ERR_LIMIT "One of the numbers exceeds the limits of an int"
-# define ERR_DOUBLE "Your list of numbers contains duplicates"
-# define ERR_EMPTY "Stack does not contain numbers"
-# define ERR_EXEC "The executable is not called push_swap"
-# define ERR_ONE "The stack contains only one number"
-/*A SUPPRIMER AVANT DE PUSH*/
+# define ERR_LINE ""
 
 typedef struct s_params
 {
